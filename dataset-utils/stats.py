4df01a81df2bfe2a6a0debece59b4e5e6092bfd1
Lucas May Petry <lucasmaypetry@gmail.com>
2018-11-20 15:05:07 -0200

    Fixed stats

--- dataset-utils/stats.py
+++ dataset-utils/stats.py
@@ -1,9 +1,7 @@
-from core.logger import Logger
 import argparse
 import numpy as np
 
 
-logger = Logger()
 arg_parser = argparse.ArgumentParser(description='Print dataset stats.')
 arg_parser.add_argument('file', help='The dataset file', type=str)
 arg_parser.add_argument('--label', default='class', help='The class column (default: class)', type=str)
@@ -31,16 +29,18 @@ avg_traj_length = str(st.mean(traj_lengths)) + \
 avg_traj_class = str(st.mean(class_counts)) + \
                     ' +- ' + str(st.stdev(class_counts))
 
-logger.log(Logger.INFO, "Number of attributes (incl. class):  " + str(num_attributes))
-logger.log(Logger.INFO, "Number of points:                    " + str(num_points))
-logger.log(Logger.INFO, "Number of trajectories:              " + str(num_trajectories))
-logger.log(Logger.INFO, "Number of classes:                   " + str(num_classes))
-logger.log(Logger.INFO, "Avg. trajectory length:              " + avg_traj_length)
-logger.log(Logger.INFO, "Avg. # of trajectories per class:    " + avg_traj_class)
-logger.log(Logger.INFO, "Max. trajectory length:              " + str(max(traj_lengths)))
-logger.log(Logger.INFO, "Min. trajectory length:              " + str(min(traj_lengths)))
-logger.log(Logger.INFO, "Largest class size:                  " + str(max(class_counts)) + " (class: " + str(labels[np.argmax(class_counts)]) + ")")
-logger.log(Logger.INFO, "Smallest class size:                 " + str(min(class_counts)) + " (class: " + str(labels[np.argmin(class_counts)]) + ")")
+print("Number of attributes (incl. class):  ", num_attributes)
+print("Number of points:                    ", num_points)
+print("Number of trajectories:              ", num_trajectories)
+print("Number of classes:                   ", num_classes)
+print("Avg. trajectory length:              ", avg_traj_length)
+print("Avg. # of trajectories per class:    ", avg_traj_class)
+print("Max. trajectory length:              ", max(traj_lengths))
+print("Min. trajectory length:              ", min(traj_lengths))
+print("Largest class size:                  ", max(class_counts), "( class:",
+      labels[np.argmax(class_counts)], ")")
+print("Smallest class size:                 ", min(class_counts), "( class:",
+      labels[np.argmin(class_counts)], ")")
 
 if args.plot:
     f = plt.figure(1)
@@ -56,5 +56,4 @@ if args.plot:
     plt.xlabel("Class size")
     plt.ylabel("Frequency")
     f2.show()
-
-    logger.get_answer('\nPress ENTER (RETURN) to exit...')
+    input('\nPress ENTER (RETURN) to exit...')
