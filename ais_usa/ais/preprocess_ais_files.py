2a40b1c16b728c8f438c03bc1099a946a05854e1
Lucas May Petry <lucasmaypetry@gmail.com>
2019-03-07 11:21:49 -0400

    Added ais scripts

--- ais_usa/ais/preprocess_ais_files.py
+++ ais_usa/ais/preprocess_ais_files.py
@@ -8,6 +8,7 @@ import pandas as pd
 proc_dir = 'processed'
 file = ':dir/:year/AIS_:year_:month_Zone:zone.csv'
 file_out = ':dir/:year/AIS_:year_:month.csv'
+vessel_file = 'vessel_types.csv'
 years = ['2015']
 months = ['01', '02', '03', '04', '05', '06',
           '07', '08', '09', '10', '11', '12']
@@ -15,6 +16,17 @@ months = ['01', '02', '03', '04', '05', '06',
 # Zones go from 01 to 20 (zones 12 and 13 do not exist)
 # Currently retrieving only the zones corresponding to the USA west coast
 zones = ['06', '07', '08', '09', '10', '11']
+
+vessels = pd.read_csv(vessel_file)
+
+
+def get_vessel_group(ntype):
+    return vessels.loc[vessels['type_number'] == ntype, 'type_group'].values[0]
+
+
+def get_vessel_desc(ntype):
+    return vessels.loc[vessels['type_number'] == ntype, 'description'].values[0]
+
 
 if not path.isdir(proc_dir):
     os.mkdir(proc_dir)
@@ -30,7 +42,8 @@ for year in years:
                           .replace(':month', month)
         fields = ['MMSI', 'DateTime', 'Lat', 'Lon', 'SOG', 'COG',
                   'Heading', 'VesselName', 'IMO', 'CallSign', 'VesselType',
-                  'Status', 'Length', 'Width', 'Draft', 'Cargo', 'Zone']
+                  'VesselTypeGroup', 'VesselTypeDescription', 'Status',
+                  'Length', 'Width', 'Draft', 'Cargo', 'Zone']
 
         with open(to_save, 'w') as o:
             writer = csv.DictWriter(o, fields)
@@ -51,6 +64,8 @@ for year in years:
                     reader = csv.DictReader(i)
 
                     for row in reader:
+                        v_type = 0 if row['VesselType'] == '' \
+                            else int(row['VesselType'])
                         writer.writerow({
                             'MMSI': row['MMSI'],
                             'IMO': 0 if row['IMO'] == '' else row['IMO'][3:],
@@ -63,8 +78,9 @@ for year in years:
                             'Heading': row['Heading'],
                             'VesselName': row['VesselName'],
                             'CallSign': row['CallSign'],
-                            'VesselType': 0 if row['VesselType'] == ''
-                                            else row['VesselType'],
+                            'VesselType': v_type,
+                            'VesselTypeGroup': get_vessel_group(v_type),
+                            'VesselTypeDescription': get_vessel_desc(v_type),
                             'Status': row['Status'],
                             'Length': row['Length'],
                             'Width': row['Width'],
